58f23583e9925fcf4582c692b487094a16d430e3
Kimhanjin <ddang0103@naver.com>
2025-03-01 21:09:16 +0900

    Feature: 유저 생성 로직 작성(DB에 인서트)
    서비스에서 레포지토리 사용시,@InjectRepository(사용할 객체명) private repo: Repository<사용할 객체명>
    으로 주입해줘야 사용가능
    컨트롤러에서는 이미 서비스에서 @Injectable() 가 선언되어있어  constructor 에만 넣어주면됨

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { create } from 'domain';
+
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password }); // create=> 생성된 엔티티 인스턴스를 반환
+    return this.repo.save(user); // DB에 실제 저장하는곳
+  }
+}
