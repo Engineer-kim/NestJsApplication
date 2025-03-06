2a65ccda344abdb00b9756e504e9e74cd3ba56be
kimhanjin <ddang0103@ziumks.com>
2025-03-06 11:17:19 +0900

    업데이트 로직 작성
    Partial<T> : 일부 속성만 선택적으로 업데이트 가능하도록 함함

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterUpdate, AfterRemove } from 'typeorm';
 
 @Entity()
 export class User {
@@ -10,4 +10,22 @@ export class User {
 
   @Column()
   password: string;
+
+  //새로운 사용자 인서트시 아래 함수 실행됨
+  @AfterInsert()
+  logInsert(){
+      console.log('Inserted User with Id ' , this.id)
+  }
+
+
+  @AfterUpdate()
+  logUpdate(){
+    console.log('Updated User with Id ' , this.id)
+  }
+
+  @AfterRemove()
+  logRemove(){
+    console.log('Removed User with Id ' , this.id)
+  }
+
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { create } from 'domain';
-
 
 @Injectable()
 export class UsersService {
@@ -13,4 +11,26 @@ export class UsersService {
     const user = this.repo.create({ email, password }); // create=> 생성된 엔티티 인스턴스를 반환
     return this.repo.save(user); // DB에 실제 저장하는곳
   }
+
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  //Partial ==> Optional
+  async update(id: number , updateInfo: Partial<User>){
+      const user = await this.findOne(id)
+      if(!user){
+        throw new Error("User Data is not Found")
+      }
+      Object.assign(user, updateInfo)
+      return this.repo.save(user)
+  }
+
+
 }
