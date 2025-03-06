b0561280b7b38033390fa36ae4190a555c111ef6
kimhanjin <ddang0103@ziumks.com>
2025-03-06 11:30:32 +0900

    사용자 삭제 로직 작성
    Delete Vs Remove
    
    - delete 는 엔티티 삭제(=> DB에서 즉시 삭제) , 반환값 없음
    
    -remove 는 삭제된 엔티티 인스턴스를 반환

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -22,15 +22,27 @@ export class UsersService {
     return this.repo.find({ where: { email } });
   }
 
-  //Partial ==> Optional
+  //Partial ==> 일부 속성만 선택적으로 업데이트 가능
   async update(id: number , updateInfo: Partial<User>){
       const user = await this.findOne(id)
       if(!user){
         throw new Error("User Data is not Found")
       }
-      Object.assign(user, updateInfo)
+      //user에 updateInfo 값을 덮어 씌우고 user 의 값이 반환된다
+      Object.assign(user, updateInfo) //원본(기존) 객체를 변경함
       return this.repo.save(user)
   }
+
+  async remove(id: number){ //Number 넘기면  객체이므로 산술 연산을 직접 수행하면 암묵적인 변환 발생 가능 => 프리미티브 타입넘길떄는 항상 조심
+    const deleteUser = await this.findOne(id)
+    if(!deleteUser){
+      throw new Error("User Data is not Found")
+    }
+    //delete 는 엔티티 삭제(=> DB에서 즉시 삭제) , 반환값 없음
+    //remove 는 삭제된 엔티티 인스턴스를 반환 
+    return this.repo.remove(deleteUser)
+  }
+    
 
 
 }
