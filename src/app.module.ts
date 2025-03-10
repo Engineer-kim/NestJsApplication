1e704102598c2921a4ab749a981a816e27025ce4
kimhanjin <ddang0103@naver.com>
2025-03-10 16:35:42 +0900

    설정 파일들 업데이트
    - OS 관계없이 설정 파일 작성 가능하게끔
    
    - dev, test 환경별로 따로 설정 파일 읽어들이게끔 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,61 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe ,MiddlewareConsumer } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+import { APP_PIPE } from '@nestjs/core';
 
+const cookieSession = require('cookie-session'); //미들웨어 가져옴
+
+// 구성 요소들에 대한 정의
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'db.sqlite',
-    entities: [User , Report],
-    synchronize: true,
-  }),UsersModule, ReportsModule],
+  imports: [
+    //ConfigModule 를 통해 .env 파일에 환경 변수를 저장하고 쉽게 불러올 수 있음
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({  //환경 변수 기반의 동적 설정
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => { //useFactory 동적으로 프로바이더(Spring에서의 서비스) 생성가능
+        return {
+          type: 'sqlite',
+          database: config.get('DB_NAME'),
+          entities: [User, Report], //자바의 그 엔티티 맞음 (DB 테이블과 매핑되는 클래스)
+          synchronize: true,
+        }
+      },
+    }),
+    // TypeOrmModule.forRoot({
+    // type: 'sqlite',
+    // database: 'db.sqlite',
+    // entities: [User , Report],
+    // synchronize: true,
+    //}),
+  UsersModule, ReportsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE, //APP_PIPE 키에 대한 전역 파이프 제공
+      useValue: new ValidationPipe({
+        whitelist: true, // DTO에 정의되지 않은 속성을 자동으로 제거
+      }),
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  //configure ==> 미들웨어 구성함수 (app.use 랑 비슷한 역할)
+  configure(consumer: MiddlewareConsumer) { //MiddlewareConsumer =>  미들웨어를 등록하고 관리하는 헬퍼 클래스
+    consumer.apply(
+      cookieSession({
+        keys: ['asdfasdf']
+      }),
+    )
+    .forRoutes('*'); //모든 라우터에 대해서 쿠키 세션의 키값을 asdfasdf로 설정
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
-const cookieSession = require('cookie-session'); //미들웨어 가져옴
 
+// app.module.ts에서 정의한 구성들을 기반으로 어플리케이션을 실행하는 진입점이다
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  app.use(cookieSession({
-    keys: ['asdfasdf']
-  }));
-  app.useGlobalPipes(
-    new  ValidationPipe({
-       whitelist: true
-    })
-  );
   // await app.listen(process.env.PORT ?? 3000);
   await app.listen(3000);
 }
