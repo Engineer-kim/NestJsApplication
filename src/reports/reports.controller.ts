0de7aabe99e0571f81e8e97fc2e7924cc3cba86a
Kimhanjin <ddang0103@naver.com>
2025-04-16 21:58:47 +0900

    @TransForm 의 데코레이터를 통한 일부 필드에 대한 형변환 및 넘어온데이터 확인인

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,32 +1,43 @@
-import { Body, Controller , Post ,UseGuards , Patch, Param } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Patch,
+  Param,
+  Get,
+  Query,
+} from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
-import { AuthGuard } from 'src/guard/auth.guard';
-import { CurrentUser } from 'src/users/decorators/current-user.decorator';
-import { User } from 'src/users/user.entity';
+import { AuthGuard } from '../guard/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
 import { ReportDto } from './dtos/reports.dto';
-import { Serialize } from 'src/interceptors/serialize.interceptor'
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { ApproveReportDto } from './dtos/approve-report.dto';
-import { AdminGuard } from 'src/guard/admin.guard';
+import { AdminGuard } from '../guard/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
-    constructor(private reportService: ReportsService) {
+  constructor(private reportsService: ReportsService) {}
 
-    }
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    console.log(query);
+  }
 
-    @Post()
-    @UseGuards(AuthGuard)
-    @Serialize(ReportDto)
-    createReports(@Body() body: CreateReportDto, @CurrentUser() user: User){
-        return this.reportService.create(body , user)
-    }
+  @Post()
+  @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
+  }
 
-
-    @Patch('/:id')
-    @UseGuards(AdminGuard)
-    approveReport(@Param('id') id: string, @Body() body: ApproveReportDto){
-        return this.reportService.changeApproval(id , body.approved)
-    }
-
+  @Patch('/:id')
+  @UseGuards(AdminGuard)
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportsService.changeApproval(id, body.approved);
+  }
 }
