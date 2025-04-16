e6189adcc622468a83b74763dff695072393baa5
Kimhanjin <ddang0103@naver.com>
2025-04-16 22:37:15 +0900

    createQueryBuilder 를 통한  DB  데이터 호출
    createQueryBuilder 이름에서도 알수 있듯 빌더 패턴으로 ,
    메소드체이닝으로 하며 createQueryBuilder쿼리 정의후 실행시 Promise 객체 반환

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm'
 import { Report } from './report.entity';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { User } from 'src/users/user.entity';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
  
 @Injectable()
 export class ReportsService {
@@ -11,6 +12,22 @@ export class ReportsService {
         @InjectRepository(Report) private repo: Repository<Report>
     ){}
 
+    createEstimate({ make, model, year, lng, lat ,mileage }: GetEstimateDto) {
+        return this.repo.createQueryBuilder()  //빌더 객체를 반환
+            .select('AVG(price)', 'price') //average price 집계 함수 결과를 price라는 이름으로 반환
+            .where('make = :make', { make })
+            .andWhere('model = :model', { model })
+            .andWhere('year = :year', { year })
+            .andWhere('lng - :lng BETWEEN -5 AND 5', { lng }) //위도 경도 차이 5도 이내
+            .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+            .andWhere('approved IS TRUE')  //andWhere => 단순히 웨어 조건을 추가하는 것
+            .orderBy('ABS(mileage - :mileage)', 'DESC') //mileage 차이의 절대값(양수)을 기준으로 내림차순 정렬
+            .setParameters({ mileage })
+            .limit(3)
+            .getRawOne()
+            //createQueryBuilder 쿼리 정의후 실행시 Promise 객체 반환 (메소드 체이닝으로 then , Catch 사용가능)
+    }
+    
     create(reportDto: CreateReportDto , user: User){
         const report = this.repo.create(reportDto)
         report.user = user
